de951f53ea9ad8970318ab70a8cd662114807d53
Jason Rhubottom <jason@rhusoft.com>
2024-08-27 16:22:00 -0700

    Generic Host

--- Program.cs
+++ Program.cs
@@ -1,38 +1,7 @@
-﻿using System.Security.Authentication;
-using System.Security.Cryptography.X509Certificates;
+using Frigate_Helper;
 
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
 
-
-namespace Frigate_Helper;
-internal class Program
-{
-    static MqttClient? mqtt;
-    readonly static EventHandler eventHandler = new();
-    private static async Task Main(string[] args)
-    {
-        mqtt = new();
-
-        mqtt.Event += e =>
-        {
-            //Handle the event
-            eventHandler.Handle(e);
-        };
-
-        try{
-            mqtt.Connect();
-        }
-        catch
-        {
-
-        }
-        
-        
-        var counter = 0;
-        var max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
-        while (max is -1 || counter < max)
-        {
-            //Console.WriteLine($"Counter: {++counter}");
-            await Task.Delay(TimeSpan.FromMilliseconds(1_000));
-        }
-    }
-}
+var host = builder.Build();
+host.Run();
---
+++ Worker.cs
@@ -0,0 +1,40 @@
+namespace Frigate_Helper;
+
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+    MqttClient mqtt= new();
+    readonly EventHandler eventHandler = new();
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+
+        mqtt.Event += e =>
+        {
+            //Handle the event
+            eventHandler.Handle(e);
+        };
+
+        try{
+            mqtt.Connect();
+        }
+        catch
+        {
+
+        }
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (_logger.IsEnabled(LogLevel.Information))
+            {
+                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            }
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
