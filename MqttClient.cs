7bdc243c2a86dcbc6f09a3a0e3770e45ab0a10c9
Jason Rhubottom <jason@rhusoft.com>
2024-08-26 21:54:56 -0700

    Moved mqtt to a class

--- MqttClient.cs
+++ MqttClient.cs
@@ -1,8 +1,33 @@
 using System;
-
+using System.Data;
+using MQTTnet.Formatter;
+using MQTTnet;
+using MQTTnet.Client;
+using System.Text;
 namespace Frigate_Helper;
 
 public class MqttClient
 {
+    MQTTnet.Client.IMqttClient? mqttClient;
+    public void Connect()
+    {
+        var mqttFactory = new MqttFactory();
 
+        mqttClient = mqttFactory.CreateMqttClient();
+        var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("home-assistant.home").WithCredentials(new MqttClientCredentials("mqtt",Encoding.ASCII.GetBytes("mqtt"))).Build();
+        //await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+        var response = mqttClient?.ConnectAsync(mqttClientOptions, CancellationToken.None);
+
+        Console.WriteLine("The MQTT client is connected.");
+
+        response.DumpToConsole();
+    }
+
+    public void Disconnect()
+    {
+        // This will send the DISCONNECT packet. Calling _Dispose_ without DisconnectAsync the
+        // connection is closed in a "not clean" way. See MQTT specification for more details.
+        mqttClient?.DisconnectAsync(new MqttClientDisconnectOptionsBuilder().WithReason(MqttClientDisconnectOptionsReason.NormalDisconnection).Build());
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,35 +1,17 @@
 ﻿using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
-using MQTTnet.Formatter;
-using MQTTnet;
-using MQTTnet.Client;
-using System.Text;
+
+
 
 namespace Frigate_Helper;
 internal class Program
 {
+    static MqttClient? mqtt;
     private static async Task Main(string[] args)
     {
-
-        var mqttFactory = new MqttFactory();
-
-        using (var mqttClient = mqttFactory.CreateMqttClient())
-        {
-            var mqttClientOptions = new MqttClientOptionsBuilder().WithTcpServer("home-assistant.home").WithCredentials(new MqttClientCredentials("mqtt",Encoding.ASCII.GetBytes("mqtt"))).Build();
-            //await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
-
-            var response = await mqttClient.ConnectAsync(mqttClientOptions, CancellationToken.None);
-
-            Console.WriteLine("The MQTT client is connected.");
-
-            response.DumpToConsole();
-
-            // This will send the DISCONNECT packet. Calling _Dispose_ without DisconnectAsync the
-            // connection is closed in a "not clean" way. See MQTT specification for more details.
-            await mqttClient.DisconnectAsync(new MqttClientDisconnectOptionsBuilder().WithReason(MqttClientDisconnectOptionsReason.NormalDisconnection).Build());
-        }
-
-
+        mqtt = new();
+        mqtt.Connect();
+        
         var counter = 0;
         var max = args.Length is not 0 ? Convert.ToInt32(args[0]) : -1;
         while (max is -1 || counter < max)
